b36412f9c0b868eac594171664e732e31bbba811
Dhruv Saxena <dhruv162002@gmail.com>
2022-03-01 13:43:17 +0530

    Initial UI

--- app/src/main/java/com/example/speechtotext/MainActivity.kt
+++ app/src/main/java/com/example/speechtotext/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.speechtotext
 
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var listeningDot : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        listeningDot = findViewById(R.id.textDot1)
+
+        ObjectAnimator.ofFloat(listeningDot, "translationY", 90f).apply {
+            duration = 5000
+            start()
+        }
+
     }
 }
